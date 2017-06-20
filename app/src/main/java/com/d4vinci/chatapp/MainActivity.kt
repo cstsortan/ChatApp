d571d3ff7cfa88838c905eb38133f03b68648e69
D4Vinci <tsortanidis.ch@gmail.com>
2017-06-20 13:38:57 +0300

    stream added, still problems with main fragment

--- app/src/main/java/com/d4vinci/chatapp/MainActivity.kt
+++ app/src/main/java/com/d4vinci/chatapp/MainActivity.kt
@@ -1,25 +1,94 @@
 package com.d4vinci.chatapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import java.util.*
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.ResultCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
+    var savedInstanceState: Bundle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.savedInstanceState = savedInstanceState
+    }
 
-        val mainFragment = MainFragment.newInstance()
+    override fun onStart() {
+        super.onStart()
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            //already signed in
+            loadMainFragment(savedInstanceState)
+        } else {
+            //not signed in
+            logInUser()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data);
+            return;
+        }
+    }
+
+    fun loadMainFragment(b: Bundle?) {
         if (findViewById(R.id.container_activity_main) != null) {
-            if (savedInstanceState != null) {
-                return
-            }
-
+            if(b!=null) return
+            val mainFragment = MainFragment.newInstance()
             supportFragmentManager
                     .beginTransaction()
                     .add(R.id.container_activity_main, mainFragment)
                     .commit()
         }
+    }
 
+    private fun logInUser() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(
+                                        AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                        AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()
+                                ))
+                        .setIsSmartLockEnabled(false)
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    private fun handleSignInResponse(resultCode: Int, data: Intent?) {
+        val response = IdpResponse.fromResultIntent(data)
+
+        // Successfully signed in
+        if (resultCode == ResultCodes.OK) {
+            loadMainFragment(savedInstanceState)
+            return
+        } else {
+            // Sign in failed
+            if (response == null) {
+                logInUser()
+                return
+            }
+
+            if (response.errorCode == ErrorCodes.NO_NETWORK) {
+                finish()
+                return
+            }
+
+            if (response.errorCode == ErrorCodes.UNKNOWN_ERROR) {
+                finish()
+                return
+            }
+        }
     }
 }
--- app/src/main/java/com/d4vinci/chatapp/MainFragment.java
+++ app/src/main/java/com/d4vinci/chatapp/MainFragment.java
@@ -1,26 +1,53 @@
 package com.d4vinci.chatapp;
 
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.firebase.ui.auth.ui.*;
 import com.firebase.ui.database.ChangeEventListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Calendar;
 
 
@@ -30,14 +57,19 @@ import java.util.Calendar;
  * create an instance of this fragment.
  */
 public class MainFragment extends Fragment {
+
     private RecyclerView recyclerView;
     private LinearLayoutManager layoutManager;
     private FirebaseRecyclerAdapter adapter;
-    private DatabaseReference mRef;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private static DatabaseReference mRef = FirebaseDatabase.getInstance().getReference();
 
     EditText etMsg;
-    EditText etName;
-    Button btSend;
+    ImageView btSend;
+    Button btMedia;
+    MediaPlayer mediaPlayer;
 
     public MainFragment() {
         // Required empty public constructor
@@ -58,19 +90,17 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_main, container, false);
-
-        mRef = FirebaseDatabase.getInstance().getReference();
-
         etMsg = (EditText) v.findViewById(R.id.et_message);
-        btSend = (Button) v.findViewById(R.id.bt_send);
-        etName = (EditText) v.findViewById(R.id.et_name);
+        btSend = (ImageView) v.findViewById(R.id.bt_send);
+        btMedia = (Button) v.findViewById(R.id.bt_media);
+
         btSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Calendar now = Calendar.getInstance();
                 if(!etMsg.getText().toString().equals("")) {
-                    mRef.child("messages").push().setValue(new Chat(etName.getText().toString(), etMsg.getText().toString(), String.valueOf(now.getTime())));
+                    mRef.child("messages").push().setValue(new Chat(user.getDisplayName().toString(), etMsg.getText().toString(), String.valueOf(now.getTime())));
                 }
                 etMsg.setText("");
                 autoScroll();
@@ -78,7 +108,32 @@ public class MainFragment extends Fragment {
         });
 
         recyclerView = (RecyclerView) v.findViewById(R.id.rv_messages);
+        initializeRecyclerView();
+        return v;
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mediaPlayer = MediaPlayer.create(getContext(), Uri.parse("http://usa2.fastcast4u.com:3684"));
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mediaPlayer.release();
+        mediaPlayer=null;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+//        mediaPlayer.release();
+//        mediaPlayer=null;
+    }
+
+    private void initializeRecyclerView() {
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true);
@@ -97,7 +152,7 @@ public class MainFragment extends Fragment {
                 chatMessageViewHolder.setText(chatMessage.getText());
                 chatMessageViewHolder.setTime(chatMessage.getTime());
                 chatMessageViewHolder.cvMsg.setRadius(32);
-                if(chatMessage.getName().equals(etName.getText().toString())) {
+                if(chatMessage.getName().equals(user.getDisplayName().toString())) {
                     chatMessageViewHolder.alignRight();
                 } else {
                     chatMessageViewHolder.alignLeft();
@@ -105,14 +160,12 @@ public class MainFragment extends Fragment {
             }
 
             @Override
-            protected void onChildChanged(ChangeEventListener.EventType type, int index, int oldIndex) {
-                super.onChildChanged(type, index, oldIndex);
+            public void onChildChanged(EventType type, DataSnapshot snapshot, int index, int oldIndex) {
+                super.onChildChanged(type, snapshot, index, oldIndex);
                 autoScroll();
             }
         };
         recyclerView.setAdapter(adapter);
-
-        return v;
     }
 
     private void autoScroll() {
@@ -126,6 +179,7 @@ public class MainFragment extends Fragment {
         TextView tv_name;
         TextView tv_time;
         CardView cvMsg;
+        ImageView ivPic;
 
         public ChatHolder(View itemView) {
             super(itemView);
@@ -134,6 +188,7 @@ public class MainFragment extends Fragment {
             cvMsg = (CardView)itemView.findViewById(R.id.cv_msg);
             tv_time = (TextView) itemView.findViewById(R.id.tv_time);
             cvMsg.setRadius(8.0f);
+            ivPic = (ImageView) itemView.findViewById(R.id.iv_prof);
         }
 
         public void setName(String name) {
@@ -150,6 +205,7 @@ public class MainFragment extends Fragment {
 
         public void alignRight() {
             cvMsg.setCardBackgroundColor(Color.YELLOW);
+            //cvMsg.setLayoutParams(new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.RIGHT));
             tv_msg.setTextColor(Color.BLACK);
             tv_name.setTextColor(Color.BLACK);
             tv_time.setTextColor(Color.BLACK);
@@ -157,6 +213,7 @@ public class MainFragment extends Fragment {
 
         public void alignLeft() {
             cvMsg.setCardBackgroundColor(Color.BLUE);
+            //cvMsg.setLayoutParams(new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.LEFT));
             tv_msg.setTextColor(Color.WHITE);
             tv_name.setTextColor(Color.WHITE);
             tv_time.setTextColor(Color.WHITE);
@@ -168,4 +225,28 @@ public class MainFragment extends Fragment {
         super.onDestroy();
         adapter.cleanup();
     }
+
+//    class RadioTask extends AsyncTask<Void, Void, Void> {
+//
+//        @Override
+//        protected Void doInBackground(Void... params) {
+//            String url = "http://usa2.fastcast4u.com:3684"; // your URL here
+//            MediaPlayer mediaPlayer = new MediaPlayer();
+//            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//            try {
+//                mediaPlayer.setDataSource(url);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                Log.d("exception", "onStart: ioException by .setDataSource() method");
+//            }
+//            try {
+//                mediaPlayer.prepare(); // might take long! (for buffering, etc)
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                Log.d("exception", "onStart: ioException by .prepare() method");
+//            }
+//            mediaPlayer.start();
+//            return null;
+//        }
+//    }
 }
